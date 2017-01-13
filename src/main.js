b2ec19fa310f5bc2e13070a7105558792772c584
Francisco Enrique Auxach <frankky96@gmail.com>
2017-01-13 16:07:13 +0100

    Added use for VueMaterial

--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ import App from './App'
 // Vue Material
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
+Vue.use(VueMaterial);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
-})
+});
