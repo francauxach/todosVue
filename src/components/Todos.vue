4fd43b2af2653e42654ca8741ab4c63b56a0296f
Francisco Enrique Auxach <frankky96@gmail.com>
2017-01-13 16:51:02 +0100

    Clean inittial code on Todos and Tokens components

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,27 +1,17 @@
 <template>
     <div>
-        <header-component/>
-        <div>this is template body</div>
-        <other-component/>
+        <div>Msg: {{ msg }}</div>
     </div>
 </template>
 <style>
-    body{
-        background-color:#ff0000;
-    }
+
 </style>
 <script>
-    import HeaderComponent from './components/header.vue'
-    import OtherComponent from './components/other.vue'
     export default{
         data(){
             return{
-                msg:'hello vue'
+                msg:'Todos aquí!'
             }
-        },
-        components:{
-            'other-component':OtherComponent,
-            HeaderComponent,
         }
     }
 </script>
