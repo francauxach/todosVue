769ccd7c67b1b35a686f669a680856fb009a8bdc
Francisco Enrique Auxach <frankky96@gmail.com>
2017-01-13 16:16:51 +0100

    Added sidenav component on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    asdasda
+  <div class="phone-viewport">
+
+    <md-toolbar>
+      <md-button class="md-icon-button" @click="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title">Todos</h2>
+    </md-toolbar>
+
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Todos</h3>
+        </div>
+      </md-toolbar>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi cupiditate esse necessitatibus beatae nobis, deserunt ut est fugit, tempora deleniti, eligendi commodi doloribus. Nemo, assumenda possimus, impedit inventore perferendis iusto!</p>
+    </md-sidenav>
+
   </div>
 </template>
 
@@ -11,6 +29,17 @@ export default {
   name: 'app',
   components: {
     Hello
+  },
+  methods: {
+    toggleLeftSidenav () {
+      this.$refs.leftSidenav.toggle()
+    },
+    open (ref) {
+      console.log('Opened: ' + ref)
+    },
+    close (ref) {
+      console.log('Closed: ' + ref)
+    }
   }
 }
 </script>
