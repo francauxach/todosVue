e570b80be5f2f954df6ec3fa8398aa9226fb46ce
Francisco Enrique Auxach <frankky96@gmail.com>
2017-01-27 16:17:57 +0100

    Added functions to get logged user

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,14 +2,15 @@
     <md-card md-with-hover>
         <md-card-header>
             <md-avatar>
-                <img src="https://s.gravatar.com/avatar/9ac3cccb8d8700f242bb89a5d31bc404?s=80" alt="People">
+                <img :src="avatar" :alt="name">
             </md-avatar>
 
-            <div class="md-title">Franc Auxach Cortés</div>
+            <div class="md-title">{{ name }}</div>
             <div class="md-subhead">Profile</div>
         </md-card-header>
 
         <md-card-content>
+            <md-spinner :md-size="150" md-indeterminate  class="md-accent" v-show="connecting" ></md-spinner>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea nostrum.
         </md-card-content>
 
@@ -22,4 +23,33 @@
 <style>
 </style>
 <script>
+  var API_PROFILE_URL = 'http://todos.dev:8000/api/v1/user'
+  var STORAGE_KEY = 'todosvue_token'
+  export default {
+    data () {
+      return {
+        avatar: 'https://s.gravatar.com/avatar/9ac3cccb8d8700f242bb89a5d31bc404?s=80',
+        name: 'Franc Auxach Cortés',
+        connecting: true
+      }
+    },
+    created () {
+      console.log('Component profile created!')
+      this.fetchUserProfile()
+    },
+    methods: {
+      fetchUserProfile: function () {
+        this.$http.defaults.headers.common['Authorization'] = 'Bearer ' + window.localStorage.getItem(STORAGE_KEY)
+        this.$http.get(API_PROFILE_URL).then((response) => {
+          this.connecting = false
+          console.log(response.data)
+        }, (response) => {
+          this.showConnectionError()
+        })
+      },
+      showConnectionError () {
+        this.$refs.connectionError.open()
+      }
+    }
+  }
 </script>
