27b159637f610df12d55a646e0f8b3fa81889d60
Francisco Enrique Auxach <frankky96@gmail.com>
2017-01-27 16:35:35 +0100

    Added functionallyties

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -6,12 +6,32 @@
             </md-avatar>
 
             <div class="md-title">{{ name }}</div>
-            <div class="md-subhead">Profile</div>
+            <div class="md-subhead">{{ email }}</div>
         </md-card-header>
 
         <md-card-content>
             <md-spinner :md-size="150" md-indeterminate  class="md-accent" v-show="connecting" ></md-spinner>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea nostrum.
+            <form novalidate @submit.stop.prevent="submit">
+                <md-input-container>
+                    <label>Name</label>
+                    <md-input v-model="name" placeholder="Put your name here" ></md-input>
+                </md-input-container>
+
+                <md-input-container>
+                    <label>Email</label>
+                    <md-input v-model="email" placeholder="Put your email here"></md-input>
+                </md-input-container>
+
+                <md-input-container>
+                    <label>Created At</label>
+                    <md-input v-model="createdAt" placeholder="Date here"></md-input>
+                </md-input-container>
+
+                <md-input-container>
+                    <label>Updated At</label>
+                    <md-input v-model="updatedAt" placeholder="Date here"></md-input>
+                </md-input-container>
+            </form>
         </md-card-content>
 
         <md-card-actions>
@@ -28,9 +48,18 @@
   export default {
     data () {
       return {
-        avatar: 'https://s.gravatar.com/avatar/9ac3cccb8d8700f242bb89a5d31bc404?s=80',
-        name: 'Franc Auxach Cortés',
+        avatar: 'https://s.gravatar.com/avatar/' + '9ac3cccb8d8700f242bb89a5d31bc4040' + '?s=80', // Ficar aquí el hash de l'email
+        id: null,
+        name: null,
+        email: null,
+        createdAt: null,
+        updatedAt: null,
         connecting: true
+      }
+    },
+    computed: {
+      avatarHash: function () {
+        return '9ac3cccb8d8700f242bb89a5d31bc4040' // S'ha de fer un md5 del email
       }
     },
     created () {
@@ -43,6 +72,11 @@
         this.$http.get(API_PROFILE_URL).then((response) => {
           this.connecting = false
           console.log(response.data)
+          this.id = response.data.id
+          this.name = response.data.name
+          this.email = response.data.email
+          this.createdAt = response.data.created_at
+          this.updatedAt = response.data.updated_at
         }, (response) => {
           this.showConnectionError()
         })
